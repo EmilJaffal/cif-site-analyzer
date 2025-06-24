1c8840b0f7de8835c7468b7a124067c8eaa5dea3
balaranjan <balaranjan@outlook.com>
2025-06-24 16:32:18 -0400

    added test for cif_reader

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,39 +1,12 @@
-import numpy as np
-import pytest
-from cif_site_analyzer import functions
+from cif_site_analyzer import cif_reader
+import os
 
 
-def test_dot_product_2D_list():
-    a = [1, 2]
-    b = [3, 4]
-    expected = 11.0
-    actual = functions.dot_product(a, b)
-    assert actual == expected
-
-
-def test_dot_product_3D_list():
-    a = [1, 2, 3]
-    b = [4, 5, 6]
-    expected = 32.0
-    actual = functions.dot_product(a, b)
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "a, b, expected",
-    [
-        # Test whether the dot product function works with 2D and 3D vectors
-        # C1: lists, expect correct float output
-        ([1, 2], [3, 4], 11.0),
-        ([1, 2, 3], [4, 5, 6], 32.0),
-        # C2: tuples, expect correct float output
-        ((1, 2), (3, 4), 11.0),
-        ((1, 2, 3), (4, 5, 6), 32.0),
-        # C3: numpy arrays, expect correct float output
-        (np.array([1, 2]), np.array([3, 4]), 11.0),
-        (np.array([1, 2, 3]), np.array([4, 5, 6]), 32.0),
-    ],
-)
-def test_dot_product(a, b, expected):
-    actual = functions.dot_product(a, b)
-    assert actual == expected
+def test_cif_readers():
+    CIF_DIR = os.path.join(os.path.dirname(__file__), "NaCl,cF8,225")
+    for cif in os.listdir(CIF_DIR):
+        if not cif.endswith("cif"):
+            continue
+        assert cif_reader.read_cif(
+            f"{CIF_DIR}{os.sep}{cif}"
+        ), f"Error reading {cif}"
